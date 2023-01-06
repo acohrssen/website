1c6ec8c4303da764f66e50e97c4345375bf24870
Amelie Cohrssen <arcohrssen@gmail.com>
2023-01-06 08:28:24 -0500

    up

---
+++ webvilletunes/playlist_store.js
@@ -0,0 +1,33 @@
+function save(item) {
+    var playlistarray = getStorearray("playlist"); playlistarray.push(item);
+    localStorage.setItem("playlist", JSON.stringify(playlistarray));
+}
+    
+
+function loadPlaylist() {
+    var playlistarray = getSavedSongs();
+    var ul = document.getElementById("playlist");
+    if (playlistarray != null) {
+        for (var i = 0; i < playlistarray.length; i++) {
+            var li = document.createElement("li"); ul.appendChild(li);
+            li.innerHTML = playlistarray[i];T 
+        }
+     }
+}
+    
+    
+
+function getSavedSongs() {
+    return getStorearray("playlist");
+}
+function getStorearray(key) {
+    var playlistarray = localStorage.getItem(key);
+    if (playlistarray == null || playlistarray == "”) {
+        playlistarray = new array();
+    }
+    else {
+        playlistarray = JSON.parse(playlistarray);
+    }
+    return playlistarray
+ 
+}
